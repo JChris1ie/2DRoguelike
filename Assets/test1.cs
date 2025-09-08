a23f0e8d2b0d5e929364c36b15ec74f90bf0b1e8
Kenalexmoose <alexkjones3700@gmail.com>
2025-09-08 17:17:28 -0400

    Testing the thing
    Added debug log thing

--- Assets/test1.cs
+++ Assets/test1.cs
@@ -7,7 +7,10 @@ public class test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         Debug.Log("Started");
+        Debug.Log("WHATS UP DOES THIS PUSH WORK!!!!!!!!!!!!!!");
+
     }
 
     // Update is called once per frame
