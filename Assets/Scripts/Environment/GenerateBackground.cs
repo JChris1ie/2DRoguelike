a4a8c9190cf7b966e11593607f0a1bdeed976ce8
JChris1ie <jeremiah.r.christie@gmail.com>
2025-09-15 10:39:28 -0400

    Backgrounds arrays
    Adds different arrays for different quarters of the background.

--- Assets/Scripts/Environment/GenerateBackground.cs
+++ Assets/Scripts/Environment/GenerateBackground.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class GenerateBackground : MonoBehaviour
 {
 
-    public GameObject[] mapPieces;
+    // Arrays for storing prefabs of background pieces
+    public GameObject[] topRight;
+    public GameObject[] topLeft;
+    public GameObject[] bottomRight;
+    public GameObject[] bottomLeft;
 
     void Awake()
     {
+        // Generates scene once program wakes
         ChangeScene();
     }
 
     public void ChangeScene()
     {
         // Method that can be called at any time to change background environment 
-        Instantiate(mapPieces[Random.Range(0,mapPieces.Length)], new Vector3(-5,5,0),Quaternion.identity);
-        Instantiate(mapPieces[Random.Range(0,mapPieces.Length)], new Vector3(5,5,0),Quaternion.identity);
-        Instantiate(mapPieces[Random.Range(0,mapPieces.Length)], new Vector3(5,-5,0),Quaternion.identity);
-        Instantiate(mapPieces[Random.Range(0,mapPieces.Length)], new Vector3(-5,-5,0),Quaternion.identity);
-        
-        
+        // Randomly selects 1 quarter of the background at a time, taking 1 piece from each array
+        Instantiate(topRight[Random.Range(0,topRight.Length)], new Vector3(-5,5,1), Quaternion.identity);
+        Instantiate(topLeft[Random.Range(0,topLeft.Length)], new Vector3(5,5,1), Quaternion.identity);
+        Instantiate(bottomRight[Random.Range(0,bottomRight.Length)], new Vector3(5,-5,1), Quaternion.identity);
+        Instantiate(bottomLeft[Random.Range(0,bottomLeft.Length)], new Vector3(-5,-5,1), Quaternion.identity);        
     }
 
 }
