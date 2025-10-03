b2ddbb50c55343e43942f29fe5dac0678164b410
Kenalexmoose <alexkjones3700@gmail.com>
2025-10-03 14:16:27 -0400

    Fixed all errors
    Fixed all errors

---
+++ Assets/Scripts/Character/Abilities/PlaceholderAbility.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderAbility : Ability
+{
+    public override void Activate(GameObject wielder)
+    {
+        Debug.Log("Hello World");
+    }
+}
