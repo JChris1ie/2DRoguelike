edd573400a3e77039291c8f9fd2f1fc096842337
Noah-Bessler <Noahbessler@gmail.com>
2025-10-22 17:04:10 -0400

    Update flipper.cs

--- Assets/flipper.cs
+++ Assets/flipper.cs
@@ -4,31 +4,23 @@ using UnityEngine;
 
 public class flipper : MonoBehaviour
 {
-    private Rigidbody2D body;
     private SpriteRenderer spriteRenderer;
     private bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D) && facingRight == false)
+        if ((Input.GetKeyDown(KeyCode.D) && facingRight == false) || (Input.GetKeyDown(KeyCode.A) && facingRight == true))
         {
-            spriteRenderer.flipX = true;
-            facingRight = true;
-            Debug.Log("flipped");
-        }
-        if (Input.GetKeyDown(KeyCode.A) && facingRight == true)
-        {
-            spriteRenderer.flipX = false;
-            facingRight = false;
-            Debug.Log("flipped");
+            facingRight = !facingRight;
+            spriteRenderer.flipX = facingRight;
+            //Debug.Log("flipped");
         }
     }
 }
